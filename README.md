016fdfbafd3e999ed75e4bf3726252824196d954
Simon McIntosh <simon.mcintosh@iter.org>
2024-10-24 15:35:35 +0900

    inital commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Nakasvr: Connect a remote kernel with a local Spyder IDE
+
+[![image](https://img.shields.io/badge/python-3.10%20%7C%203.11%20%7C%203.13-blue)](https://git.iter.org/projects/EQ/repos/nova)
+#[![image](https://img.shields.io/badge/license-IO%20GIP-green)]()
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/charliermarsh/ruff/main/assets/badge/v2.json)](https://github.com/charliermarsh/ruff)
+
+Nakasvr helps you install, launch, and connect remote python kernels to your local Spyder IDE
+
+## Installation
+Instalation is required on both the client (local) and server (remote).
+
+The use of virtual environments is encoraged.
+
+Create virtual environments for the client and server. Subsiquent use of python commands assumes
+that the virtual environment is active.
+
+Installation on the server requires a recent Python>=3.10 module.
+```sh
+module use /home/d230021/public/imas/etc/modules/all
+ml PythonNakasvr helps you install, lanuch, and connect remote python kernels to your local Spyder IDE
+```
+
+```sh
+python -m venv ~/.venv/nakasvr
+. ~/.venv/nakasvr/bin/activate
+```
+
+Client
+```sh
+pip install .[client]
+```
+
+Server
+```sh
+pip install .[server]
+```
+
+## Use
+Connection of a local Spyder IDE to a remote client requires the lanuch of a Spyder kernel on the server side.
+This is managed using the following script
+
+###
+```sh
+
+launch_kernel
+```
+
+## Development
+This packaging and development is managed using Poetry>=2.
+
+```sh
+pipx install git+https://github.com/python-poetry/poetry.git
+pipx inject poetry "poetry-dynamic-versioning[plugin]"
+
+poetry install
+pre-commit install
+```
---
+++ nakasvr/kernel.py
@@ -0,0 +1,154 @@
+"""Lanuch spyder kernel on remote server."""
+
+from dataclasses import dataclass, field
+from functools import cached_property
+import invoke
+from pathlib import Path
+import subprocess
+
+import appdirs
+import click
+import fabric
+import fsspec
+import logging
+import numpy as np
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+@dataclass
+class Connect:
+    """Manage connection to remote server."""
+
+    username: str
+    hostname: str = "nakasvr23.naka.qst.go.jp"
+    session: str = "nakasvr"
+
+    def has_session(self, ssh):
+        """Return True if has-session else create new session."""
+        try:
+            result = ssh.run(f"tmux has-session -t {self.session}", hide=True)
+            assert result.return_code == 0
+            return True
+        except (invoke.UnexpectedExit, AssertionError):
+            ssh.run(f"tmux new -d -s {self.session} /usr/bin/bash", hide=True)
+
+    @cached_property
+    def filesystem(self):
+        """Return fsspec instance."""
+        return fsspec.filesystem("ssh", host=self.hostname, username=self.username)
+
+    def connect(self):
+        """Mannage connection to host."""
+        return fabric.Connection(f"{self.username}@{self.hostname}")
+
+    def kill(self):
+        """Kill remote tmux session."""
+        with self.connect() as ssh:
+            ssh.run(f"tmux kill-session -t {self.session}")
+
+
+@dataclass
+class SpyderKernel(Connect):
+    """Manage remote spyder-kernel."""
+
+    kerneldir: str = field(default_factory=appdirs.user_cache_dir)
+
+    @cached_property
+    def venv_dir(self):
+        """Return venv directory."""
+        try:
+            venv_dir = f"/home/{self.username}/.venv/{self.session}/bin"
+            assert self.filesystem.isdir(venv_dir)
+        except AssertionError as error:
+            raise AssertionError(
+                "SpyderKernel requires a venv on the "
+                "remote host with a compatatable version of "
+                "spyder-kernel installed."
+            ) from error
+        return venv_dir
+
+    def _ssh_run(self, ssh, command):
+        """Run tmux command."""
+        command = command.replace(" ", r"\ ")
+        return ssh.run(rf"tmux send -t {self.session}.0 {command} ENTER", hide=True)
+
+    def launch(self):
+        """Launch spyder kernel in tmux session running in a venv on the remote host."""
+        module_use = "ml use /home/d230021/public/imas/etc/modules/all"
+        module_load = "ml Python"
+        venv_activate = f". {self.venv_dir}/activate"
+        spyder_kernel = "python -m spyder_kernels.console"
+
+        with self.connect() as ssh:
+            if self.has_session(ssh):
+                return
+            for command in [module_use, module_load, venv_activate, spyder_kernel]:
+                self._ssh_run(ssh, command)
+
+    @cached_property
+    def runtime(self):
+        """Return runtime directory."""
+        runtime = f"/home/{self.username}/.local/share/jupyter/runtime"
+        try:
+            assert self.filesystem.isdir(runtime)
+        except AssertionError as error:
+            raise FileNotFoundError(
+                f"runtime directory {runtime} not found."
+            ) from error
+        return runtime
+
+    def locate(self):
+        """Return file detal of last created kernel."""
+        files = self.filesystem.ls(self.runtime, detail=True)
+        filedetail = files[np.argmax([file["mtime"].timestamp() for file in files])]
+        logging.info(f'Found {Path(filedetail["name"]).name}.')
+        logging.info(f'Kernel last modified at {filedetail["time"]}.')
+        return filedetail
+
+    def copy(self):
+        """Copy remote kernel connection file to local host."""
+        filedetail = self.locate()
+        filepath = filedetail["name"]
+        filename = Path(filepath).name
+        try:
+            assert self.filesystem.isfile(filepath)
+        except AssertionError as error:
+            raise FileNotFoundError(f"kernel file {filename} not found.") from error
+
+        kerneldir = Path(self.kerneldir) / "kernel.json"
+        subprocess.run(
+            ["scp", f"{self.username}@{self.hostname}:{filepath}", f"{kerneldir}"]
+        )
+        assert kerneldir.is_file()
+        logging.info(f"Copied {filename} from {self.hostname} to {kerneldir}.")
+
+
+@click.command()
+@click.option(
+    "--username",
+    default=None,
+    help="Set username for connection to remote host.",
+)
+@click.option(
+    "--hostname", default="nakasvr23.naka.qst.go.jp", help="Address for remote host."
+)
+def remote_kernel(username, hostname):
+    """Launch remote spyder-kernel and copy to local host."""
+    kernel = SpyderKernel(username, hostname)
+    kernel.launch()
+    kernel.copy()
+
+
+if __name__ == "__main__":
+
+    kernel = SpyderKernel("d240044", "nakasvr23.naka.qst.go.jp")
+    kernel.launch()
+    kernel.copy()
